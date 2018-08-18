6228474acf6667429dd4da4b29808ac98477c327
LuisM000 <luis.m.r@outlook.com>
2018-08-18 11:11:40 +0200

    Console sample added

---
+++ DynamicProxySamples.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DynamicProxySamples.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimingExtension.Configure(new ConsoleWriter());
+            var sleepyhead = new Sleepyhead().AddTiming();
+            sleepyhead.Sleep();
+            System.Console.ReadLine();
+        }
+    }
+
+    public class Sleepyhead
+    {
+        public virtual void Sleep()
+        {
+            Thread.Sleep(500);
+        }
+    }
+
+    class ConsoleWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            System.Console.WriteLine(text);
+        }
+    }
+}
